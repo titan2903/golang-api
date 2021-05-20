6c797c898f17c7a3c8c389333a4c1a7169df1fdd
Titanio Yudista <titanioyudista98@gmail.com>
2021-05-20 21:51:52 +0700

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bwastartup/user"
 	"fmt"
 	"log"
 
@@ -11,11 +12,25 @@ import (
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:user1234@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connected...")
+
+	var users []user.User
+	length := len(users)
+	fmt.Println(length)
+
+	db.Find(&users) //! type harus pointer
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+	}
 }
