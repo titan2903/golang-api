0a72b4d0d02d0a8a431d449f0dce1d3561680b21
Titanio Yudista <titanioyudista98@gmail.com>
2021-05-24 14:57:45 +0700

    campaign service, get campaign

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,30 @@
 package campaign
+
+type Service interface {
+	FindCampaigns(UserID int) ([]Campaingn, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) FindCampaigns(userID int) ([]Campaingn, error) {
+	//! melakukan switching percabangan
+	if userID != 0 {
+		campaigns, err := s.repository.FindByUserID(userID)
+		if err != nil {
+			return campaigns, err
+		}
+		return campaigns, nil
+	} else {
+		campaigns, err := s.repository.FindAll()
+		if err != nil {
+			return campaigns, err
+		}
+		return campaigns, nil
+	}
+}
