bfebaaa116dc2d116cc3f61816dfd33a5fbdbd27
Titanio Yudista <titanioyudista@refactory.id>
2021-09-14 13:38:52 +0700

    fixing

--- user/input.go
+++ user/input.go
@@ -3,22 +3,22 @@ package user
 type RegisterUserInput struct { //! struct yang digunakan untuk mapping dari inputan user
 	Name       string `json:"name" binding:"required"`
 	Occupation string `json:"occupation" binding:"required"`
-	Email      string `json:"email" binding:"required,email"`
+	Email      string `json:"email" binding:"required"`
 	Password   string `json:"password" binding:"required"`
 }
 
 type LoginInput struct {
-	Email    string `json:"email" form:"email" binding:"required,email"`
+	Email    string `json:"email" form:"email" binding:"required"`
 	Password string `json:"password" form:"password" binding:"required"`
 }
 
 type CheckEmailInput struct {
-	Email string `json:"email" binding:"required,email"`
+	Email string `json:"email" binding:"required"`
 }
 
 type FormCreateUserInput struct {
 	Name       string `form:"name" binding:"required"`
-	Email      string `form:"email" binding:"required,email"`
+	Email      string `form:"email" binding:"required"`
 	Occupation string `form:"occupation" binding:"required"`
 	Password   string `form:"password" binding:"required"`
 	Error      error
@@ -27,7 +27,7 @@ type FormCreateUserInput struct {
 type FormUpdateUserInput struct {
 	ID         int
 	Name       string `form:"name" binding:"required"`
-	Email      string `form:"email" binding:"required,email"`
+	Email      string `form:"email" binding:"required"`
 	Occupation string `form:"occupation" binding:"required"`
 	Error      error
 }
